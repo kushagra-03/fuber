bf8c37ac24ae3de6b8ac543449b026fbeda9f837
Kushagra singh <kushagr.singh3@gmail.com>
2019-11-12 14:11:37 +0530

    added high level spec to run the application from the clients perspective

--- spec/fuber_spec.rb
+++ spec/fuber_spec.rb
@@ -16,4 +16,20 @@ describe Fuber do
 			expect(last_response.status).to eq 200
 		end
 	end
+
+	describe '#POST /bookings' do
+		context 'when booking is created successfully' do
+			
+			let(:cab) { Cab.new(registration_number: '12322', longitude: 123231, latitude: 123213) }
+			let(:customer) { Customer.new(customer_id: 1, longitude: 100001, latitude: 100002) }
+
+			before do
+				post '/bookings', params: { customer_id: 1, hipster: false }
+			end
+
+			it 'returns status created' do
+				expect(last_response.status).to eq :created
+			end
+		end
+	end
 end
