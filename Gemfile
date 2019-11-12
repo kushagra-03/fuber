d74667ece5343858c7ee24db5281a5b1a054fbd3
Kushagra singh <kushagr.singh3@gmail.com>
2019-11-12 15:04:20 +0530

    added code to fix funtional spec

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'thin'
-
+gem 'json'
+gem 'pry'
 group :test do
   gem 'rack-test'
   gem 'rspec'
--- fuber.rb
+++ fuber.rb
@@ -1,12 +1,24 @@
 require 'sinatra'
+require 'json'
+require 'pry'
 
 class Fuber < Sinatra::Base
+
+	before do
+	  content_type :json
+	end
+
 	get '/' do
 		'Welcoome to Fuber'
 	end
 
-  get '/bookings' do
-		'List of all bookings'
+  post '/bookings' do
+		@booking = ::Booking.new(**params)
+
+		if @booking
+			status '201'
+			@booking.to_json
+		end
 	end
 end
 
