ee635c469e4fc5e00bd7e5137f3b97228735218f
Kushagra singh <kushagr.singh3@gmail.com>
2019-11-12 15:33:40 +0530

    added customer and cab to fix failing specs

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -4,17 +4,17 @@ require_relative '../../app/models/booking'
 describe Booking do
 	subject { described_class.new(customer: customer, cab: cab) }
 
-	describe '#customer' do
-		let!(:customer) { Customer.new(customer_id: 1, longitude: 100001, latitude: 100002) }
+	let!(:customer) { Customer.new(customer_id: 1, longitude: 100001, latitude: 100002) }
+	let!(:cab) { Cab.new(registration_number: 1, longitude: 100001, latitude: 100002) }
+	
 
+	describe '#customer' do
 		it 'returns the customer' do
 			expect(subject.customer).to eq customer
 		end
 	end
 
 	describe '#cab' do
-		let!(:cab) { Cab.new(registration_number: 1, longitude: 100001, latitude: 100002) }
-
 		it 'returns the cab' do
 			expect(subject.cab).to eq cab
 		end
