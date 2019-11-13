30081d9c8a4ef4f07c5f3475621a0ad5c22fd692
Kushagra singh <kushagr.singh3@gmail.com>
2019-11-13 20:43:58 +0530

    updated fuber.rb and spec to support ending of bookings

--- fuber.rb
+++ fuber.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 require 'json'
 require 'cmath'
 require 'pry'
+require './app/models/customer'
+require './app/models/location'
+require './app/models/nearest_cab_finder'
+require './app/models/booking'
+require './app/models/cab'
+require './app/models/pink_cab'
 
 class Fuber < Sinatra::Base
 
@@ -9,19 +15,50 @@ class Fuber < Sinatra::Base
 	  content_type :json
 	end
 
-	get '/' do
-		'Welcoome to Fuber'
-	end
-
   post '/bookings' do
-  	@customer = Customer.find(params[:customer_id])
-		@booking = ::Booking.new(**params)
+  	@customer = ::Customer.find(params[:customer_id])
 
-		if @booking
+  begin
+		@booking = ::Booking.new(customer: @customer)
+	rescue ::Booking::NilCustomerSupplied
+		render_booking_not_created
+	else
+		@booking.locate_cab(cabs: ::Cab.available, hipster: hipster?)
+
+		if @booking.fulfilled?
 			status '201'
 			@booking.to_json
+		else
+			render_booking_not_created
 		end
 	end
-end
+	end
 
-#run Fuber.run!
+  put '/bookings/:customer_id' do
+  	@booking = Booking.all.find { |booking| booking.customer.id == params[:customer_id]}
+
+  	if @booking
+  		status '200'
+  		@booking.end
+  		@booking.to_json
+  	else
+  		render_booking_not_found
+  	end
+	end
+
+	private
+
+	def hipster?
+		params[:hipster] == 'true'
+	end
+
+	def render_booking_not_found
+		status '404'
+		{ message: "Booking could not be found" }.to_json
+	end
+
+	def render_booking_not_created
+		status '422'
+		{ message: "Booking could not be created" }.to_json
+	end
+end
--- spec/fuber_spec.rb
+++ spec/fuber_spec.rb
@@ -9,26 +9,112 @@ describe Fuber do
 	  Fuber
 	end
 
-	describe '#GET /' do
-		it 'returns status 200 OK' do
-			get '/'
+	describe '#POST /bookings' do
+		context 'when booking is created successfully' do
+			context 'when booking is not hipster' do
+				let!(:cab) { Cab.new(registration_number: '1', location: Location.new(longitude: 123231, latitude: 123213)) }
+				let!(:other_cab) { Cab.new(registration_number: '2', location: Location.new(longitude: 231, latitude: 3213)) }
+				let!(:another_cab) { Cab.new(registration_number: '3', location: Location.new(longitude: 230, latitude: 3210)) }
 
-			expect(last_response.status).to eq 200
+				let(:json_response) { JSON.load(last_response.body) }
+
+				before do
+					another_cab.occupy
+					Cab.all = [cab, other_cab, another_cab]
+
+					post '/bookings', customer_id: 1, hipster: false
+				end
+
+				it 'returns status created' do
+					expect(last_response.status).to eq 201
+				end
+
+				it 'returns the booking created' do
+					expect(json_response['cab']).to eq '2'
+					expect(json_response['customer']).to eq '1'
+				end
+			end
+
+			context 'when booking is hipster' do
+				let!(:cab) { Cab.new(registration_number: '1', location: Location.new(longitude: 123231, latitude: 123213)) }
+				let!(:other_cab) { Cab.new(registration_number: '2', location: Location.new(longitude: 231, latitude: 3213)) }
+				let!(:pink_cab) { PinkCab.new(registration_number: '3', location: Location.new(longitude: 231, latitude: 3213)) }
+
+				let(:json_response) { JSON.load(last_response.body) }
+
+				before do
+					Cab.all = [cab, other_cab, pink_cab]
+
+					post '/bookings', customer_id: 1, hipster: true
+				end
+
+				it 'returns status created' do
+					expect(last_response.status).to eq 201
+				end
+
+				it 'returns the booking created' do
+					expect(json_response['cab']).to eq '3'
+					expect(json_response['customer']).to eq '1'
+				end
+			end
+		end
+
+		context 'when booking is not created' do
+			
+			let!(:cab) { Cab.new(registration_number: '12322', location: Location.new(longitude: 123231, latitude: 123213)) }
+			let!(:other_cab) { Cab.new(registration_number: '12329', location: Location.new(longitude: 231, latitude: 3213)) }
+
+			let(:json_response) { JSON.load(last_response.body) }
+
+			before do
+				Cab.all = [cab, other_cab]
+
+				post '/bookings', hipster: false
+			end
+
+			it 'returns status unprocessible entity' do
+				expect(last_response.status).to eq 422
+			end
 		end
 	end
 
-	describe '#POST /bookings' do
-		context 'when booking is created successfully' do
-			
-			let(:cab) { Cab.new(registration_number: '12322', longitude: 123231, latitude: 123213) }
-			let(:customer) { Customer.new(customer_id: 1, longitude: 100001, latitude: 100002) }
+	describe '#PUT bookings/:customer_id' do
+		context 'when booking is updated' do
+			let!(:cab) { Cab.new(registration_number: '1', location: Location.new(longitude: 123231, latitude: 123213)) }
+			let!(:other_cab) { Cab.new(registration_number: '2', location: Location.new(longitude: 231, latitude: 3213)) }
+			let!(:another_cab) { Cab.new(registration_number: '3', location: Location.new(longitude: 230, latitude: 3210)) }
+			let!(:booking) { Booking.new(customer: Customer.find('1')) }
+			let(:json_response) { JSON.load(last_response.body) }
 
 			before do
-				post '/bookings', customer_id: 1, hipster: false
+				another_cab.occupy
+				Cab.all = [cab, other_cab, another_cab]
+
+				booking.locate_cab(cabs: Cab.available)
+
+				Booking.all = [booking]
+				put '/bookings/1'
 			end
 
-			it 'returns status created' do
-				expect(last_response.status).to eq :created
+			it 'returns status updated' do
+				expect(last_response.status).to eq 200
+
+				expect(json_response['cab']).to eq '2'
+				expect(json_response['customer']).to eq '1'
+			end
+		end
+
+		context 'when booking is not updated' do
+			let!(:booking) { Booking.new(customer: Customer.find('1')) }
+			let(:json_response) { JSON.load(last_response.body) }
+
+			before do
+				Booking.all = [booking]
+				put '/bookings/2'
+			end
+
+			it 'returns status not found' do
+				expect(last_response.status).to eq 404
 			end
 		end
 	end
