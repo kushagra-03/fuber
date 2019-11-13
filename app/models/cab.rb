1947a6503db7861fc6a612eecfb292ded2a7207a
Kushagra singh <kushagr.singh3@gmail.com>
2019-11-13 20:41:41 +0530

    added logic to find distance from a location and toggle occupied or not

--- app/models/cab.rb
+++ app/models/cab.rb
@@ -1,17 +1,48 @@
 class Cab
-	attr_reader :registration_number, :location
+	attr_reader :registration_number, :occupied
+	attr_accessor :location
 
 	def self.all
 		@@all ||= [
-			new(registration_number: 123, location: Location.new(latitude: 123345, longitude: 11233232)),
-			new(registration_number: 124, location: Location.new(latitude: 123344, longitude: 11233233)),
-			new(registration_number: 125, location: Location.new(latitude: 123341, longitude: 11233234)),
+			new(registration_number: 123, location: Location.new(latitude: 14, longitude: 120)),
+			new(registration_number: 124, location: Location.new(latitude: 16, longitude: 100)),
+			new(registration_number: 125, location: Location.new(latitude: 17, longitude: 120)),
+			PinkCab.new(registration_number: 126, location: Location.new(latitude: 17, longitude: 120))
 		]
+	end
+
+	def self.available
+		all.select { |cab| cab.occupied == false }
+	end
+
+	def self.all=(cabs)
+		@@all = cabs
+	end
+
+	def self.hipster
+		all.select(&:hipster?)
 	end
 
 	def initialize registration_number:, location:
 		@registration_number = registration_number
 		@location = location
+		@occupied = false
 	end
 
+	def occupy
+		@occupied = true
+	end
+
+	def unoccupy(location)
+		self.location = location
+		@occupied = false
+	end
+
+	def distance_from locatable
+		location.distance_from locatable
+	end
+
+	def hipster?
+		false
+	end
 end
--- spec/models/cab_spec.rb
+++ spec/models/cab_spec.rb
@@ -2,7 +2,31 @@ require 'spec_helper'
 require_relative '../../app/models/cab'
 
 describe Cab do
-	subject { described_class.new(registration_number: '12322', location: Location.new(longitude: 123231, latitude: 123213)) }
+	subject { described_class.new(registration_number: '12322', location: Location.new(longitude: 97, latitude: 19)) }
+
+	describe '.available' do
+		before do
+			subject.occupy
+
+			Cab.all = [subject]
+		end
+
+		it 'returns available cabs' do
+			expect(Cab.available).to eq []
+		end
+	end
+
+	describe '.available' do
+		subject { PinkCab.new(registration_number: '12322', location: Location.new(longitude: 97, latitude: 19)) }
+
+		before do
+			Cab.all = [subject]
+		end
+
+		it 'returns available cabs' do
+			expect(Cab.hipster).to eq [subject]
+		end
+	end
 
 	describe '#registration_number' do
 		it 'returns the registration_number' do
@@ -15,4 +39,52 @@ describe Cab do
 			expect(subject.location).to be_a(Location)
 		end
 	end
+
+	describe '#occupied' do
+		it 'returns the occupied' do
+			expect(subject.occupied).to eq(false)
+		end
+	end
+
+	describe '#occupy' do
+		before do
+			subject.occupy
+		end
+
+		it 'sets occupied to true' do
+			expect(subject.occupied).to eq(true)
+		end
+	end
+
+	describe '#hipster?' do
+		it 'returns false' do
+			expect(subject.hipster?).to eq false
+		end
+	end	
+
+	describe '#unoccupy' do
+		let(:location) { Location.new(latitude: 1, longitude: 167) }
+
+		before do
+			subject.occupy
+
+			subject.unoccupy(location)
+		end
+
+		it 'sets occupied to false' do
+			expect(subject.occupied).to eq(false)
+		end
+
+		it 'sets occupied to false' do
+			expect(subject.location).to eq(location)
+		end
+	end	
+
+	describe '#distance_from' do
+		let(:location) { Location.new(latitude: 1, longitude: 167) }
+
+		it 'returns the distance from the location passed' do
+			expect(subject.distance_from(location)).to eq 72.27724399837061
+		end
+	end	
 end
