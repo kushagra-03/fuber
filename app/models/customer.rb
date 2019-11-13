8bebace8ebeb840612451036fa562ef86eb20196
Kushagra singh <kushagr.singh3@gmail.com>
2019-11-13 20:42:34 +0530

    added logic to find a particular customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,22 +1,18 @@
 class Customer
 	attr_reader :id, :location
 
-
 	def self.all
 		@@all ||= [
-			new(id: 1, location: Location.new(latitude: 12345, longitude: 1133232)),
-			new(id: 2, location: Location.new(latitude: 13344, longitude: 1133233)),
-			new(id: 3, location: Location.new(latitude: 13341, longitude: 1133234)),
+			new(id: '1', location: Location.new(latitude: 23, longitude: 113)),
 		]
 	end
 
 	def self.find(id)
-		@all.find { |customer| customer.id == id }
+		all.find { |customer| customer.id == id }
 	end
 
 	def initialize(id:, location:)
 		@id = id
 		@location = location
 	end
-
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,6 +4,12 @@ require_relative '../../app/models/customer'
 describe Customer do
 	subject { described_class.new(id: '12322', location: Location.new(longitude: 123231, latitude: 123213)) }
 
+	describe '.find' do
+		it 'returns the customer with the supplied id' do
+			expect(described_class.find('1').id).to eq '1'
+		end
+	end
+
 	describe '#id' do
 		it 'returns the id' do
 			expect(subject.id).to eq '12322'
